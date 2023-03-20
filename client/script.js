6138e26c246e171403778c279a386027994c32e3
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-03-20 16:23:14 +0000

    Fix chat_container type

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_containerr');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
