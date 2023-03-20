2947de79d4d05d3461193b654e4ac5cb38ebbaac
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-03-20 15:15:48 +0000

    Add text increment to the typeText function for when the AI is generating response

--- client/script.js
+++ client/script.js
@@ -23,8 +23,17 @@ function typeText(element, text) {
   let interval = setInterval(() => {
     if (index < text.length) {
       element.innerHTML += text.charAt(index);
+      index++;
     } else {
       clearInterval(interval);
     }
   }, 20)
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
 }
